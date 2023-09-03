8d8a8063a1f2307c657677684afdfaa152e4cd43
Akshay Lakade <akshaylakade5678@gmail.com>
2023-09-03 15:34:49 +0530

    Update operator_dependent.sh

--- Linux System /Assignments/operator_dependent.sh
+++ Linux System /Assignments/operator_dependent.sh
@@ -20,10 +20,50 @@ Documentation
 # Main Program Starts from here
 
 str=$@
+opr=`echo ${str: -1}`
+len=`expr ${#str} - 2`
+tmp=`echo ${str:0:1}`
 
 if [ $1 ]
 then
-    echo ${str[@]: 1}
+    case $opr in
+        +)
+            for i in `seq 1 $len`
+            do 
+                digit=`echo ${str:$i:1}`
+                tmp=`expr $tmp + $digit`
+            done
+            echo "Sum of digits = $tmp"
+        ;;
+        -)
+            for i in `seq 1 $len`
+            do 
+                digit=`echo ${str:$i:1}`
+                tmp=`expr $tmp - $digit`
+            done
+            echo "Subtraction of digits = $tmp"
+        ;;
+        /)
+            for i in `seq 1 $len`
+            do 
+                digit=`echo ${str:$i:1}`
+                tmp=`echo "scale=2;"$tmp/$digit" " | bc`
+            done
+            echo "Division of digits = $tmp"
+        ;;
+        x)
+            for i in `seq 1 $len`
+            do 
+                digit=`echo ${str:$i:1}`
+                tmp=$(($tmp*$digit)) 
+            done
+            echo "Multiplication  of digits = $tmp"
+        ;;
+        *)
+            echo "Error: Operator missing or invalid operator, please pass operator as last digit (+,-,x,/)"
+        ;;
+
+    esac
 else
     echo "Error : Please pass the arguments through CL."
     echo "Usage : ./operator_dependent.sh 2345+"
