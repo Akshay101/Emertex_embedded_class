6b342d2b745a8b66f410eb8a828a224267d12857
Akshay Lakade <akshaylakade5678@gmail.com>
2023-09-03 14:37:49 +0530

    Add files via upload

---
+++ Linux System /Example/function_ex.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+function display()
+{
+    echo "Inside Function"
+}
+
+echo "Hello"
+display
+echo "Outside Function"
---
+++ Linux System /Example/pattern_3_ex.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+                                  
+for i in `seq 5`  
+do                            
+  for j in `seq  $i`                                 
+  do
+    echo -n "* "                                                                    
+  done                                                                                
+  echo                                              
+done                                              
